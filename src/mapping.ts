be1e5fe02788cb3e6f8517d2fc534cd223447432
NoxApi <pcsnon@gmail.com>
2022-04-19 05:24:37 +0700

    beta1

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,83 +1,48 @@
 import { BigInt } from "@graphprotocol/graph-ts"
-import {
-  minichef,
-  Deposit,
-  EmergencyWithdraw,
-  Harvest,
-  LogEvermoonPerSecond,
-  LogPoolAddition,
-  LogSetPool,
-  LogUpdatePool,
-  OwnershipTransferred,
-  Withdraw
-} from "../generated/minichef/minichef"
-import { ExampleEntity } from "../generated/schema"
+import { Deposit,EmergencyWithdraw,Withdraw } from "../generated/minichef/minichef"
+import { Users,TVL } from "../generated/schema"
 
 export function handleDeposit(event: Deposit): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.user = event.params.user
+  let entity = Users.load(event.params.user.toString())
+  if (entity===null)
+    entity = new Users(event.params.user.toString())
   entity.pid = event.params.pid
-
-  // Entities can be written to the store with `.save()`
+  const storeamount = entity.amount
+  const amount =event.params.amount
+  entity.amount = storeamount+amount
   entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.EVERMOON(...)
-  // - contract.evermoonPerSecond(...)
-  // - contract.lpToken(...)
-  // - contract.owner(...)
-  // - contract.pendingEvermoon(...)
-  // - contract.pendingOwner(...)
-  // - contract.poolInfo(...)
-  // - contract.poolLength(...)
-  // - contract.rewarder(...)
-  // - contract.totalAllocPoint(...)
-  // - contract.updatePool(...)
-  // - contract.userInfo(...)
+  let total = TVL.load("1")
+  if (total===null)
+  total = new TVL("1")
+  total.amount += event.params.amount
+  total.save()
+}
+export function handleEmergencyWithdraw(event: EmergencyWithdraw): void {
+  let entity = Users.load(event.params.user.toString())
+  if (entity===null)
+    entity = new Users(event.params.user.toString())
+  entity.pid = event.params.pid
+  entity.amount -= event.params.amount
+  entity.save()
+  let total = TVL.load("1")
+  if (total===null)
+  total = new TVL("1")
+  total.amount = -(event.params.amount)+total.amount
+  total.save()
 }
 
-export function handleEmergencyWithdraw(event: EmergencyWithdraw): void {}
-
-export function handleHarvest(event: Harvest): void {}
-
-export function handleLogEvermoonPerSecond(event: LogEvermoonPerSecond): void {}
-
-export function handleLogPoolAddition(event: LogPoolAddition): void {}
-
-export function handleLogSetPool(event: LogSetPool): void {}
-
-export function handleLogUpdatePool(event: LogUpdatePool): void {}
-
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
-
-export function handleWithdraw(event: Withdraw): void {}
+export function handleWithdraw(event: Withdraw): void {
+  let entity = Users.load(event.params.user.toString())
+  if (entity===null)
+    entity = new Users(event.params.user.toString())
+  entity.pid = event.params.pid
+  entity.amount = -(event.params.amount)+entity.amount
+  entity.save()
+  let total = TVL.load("1")
+  if (total===null)
+  total = new TVL("1")
+  total.amount = -(event.params.amount)+total.amount
+  total.save()
+}
